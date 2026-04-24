690d0f86bd65f9302cbac931024929d69086d970
Ebintoly <pk22272@bristol.ac.uk>
2026-04-24 17:12:35 +0100

    Pretty cool animations done

--- game/Assets/Bhaptics/SDK2/Scripts/AutoGenerated/BhapticsEvent.cs
+++ game/Assets/Bhaptics/SDK2/Scripts/AutoGenerated/BhapticsEvent.cs
@@ -30,5 +30,7 @@ namespace Bhaptics.SDK2
         public const string HEARTBEAT_BUZZ = "heartbeat_buzz";
         
         public const string HUNT_VIBRATION = "hunt_vibration";
+        
+        public const string MONSTER_SLASH = "monster-slash";
     }
 }
