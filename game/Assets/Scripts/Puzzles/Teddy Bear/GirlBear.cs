25eb0a9b0d757add019e565c3eebf515f0b52878
justice-123 <mintah166@gmail.com>
2026-04-30 10:44:26 +0100

    hallucination disabled on rlgl load and tox fill rate to 2

--- game/Assets/Scripts/Puzzles/Teddy Bear/GirlBear.cs
+++ game/Assets/Scripts/Puzzles/Teddy Bear/GirlBear.cs
@@ -36,6 +36,10 @@ public class GirlBear : MonoBehaviour
     private bool pauseStarted = false;
     private Renderer[] renderers;
 
+
+    [Header("Disable hallucinations ")]
+    [SerializeField] private GameObject hallucinationToDisable;
+
     void Start()
     {
         renderers = GetComponentsInChildren<Renderer>();
@@ -126,6 +130,9 @@ public class GirlBear : MonoBehaviour
         axeSpawned = true;
         SpawnAxeFromBehindPlayer();
         yield return new WaitForSeconds(1.0f);
+
+        hallucinationToDisable.SetActive(false);
+
 
         MySceneManager.Instance.LoadNewScene("Final-monster-game-scene");
 
